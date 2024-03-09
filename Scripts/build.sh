413a8c7f72438cc1c832cfd7cb7edb79f4b71891
David Leclerc <d.leclerc.pro@gmail.com>
2024-03-09 03:11:44 +0100

    New app name.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -5,7 +5,7 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Define image variables
 user="dleclercpro"
-app="ssl-reverse-proxy"
+app="reverse-proxy"
 release="latest"
 
 # Build a cross-platform image and push it to Dockerhub
